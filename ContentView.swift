b44faa2a8b024770f7c0169acf98687a42e8c3c2
shanejcoffey <shanejcoffey27@gmail.com>
2025-12-10 08:09:12 -0600

    did lots of stuff

--- ContentView.swift
+++ ContentView.swift
@@ -1,13 +1,92 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var numCards: Int = 4
+    
+    let difficultiesDictionary: [String: Int] = [
+        "super easy": 4,
+        "easy": 8,
+        "medium": 16,
+        "hard": 24
+    ]
+    
+    @State var difficultySelected: String = "super easy"
+    
     var body: some View {
         NavigationStack {
             VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
+                Spacer()
+                
+                Text("Welcome to memory game")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Spacer()
+                
+                Text("select num cards here")
+                
+                ZStack {
+                    Menu {
+                        // this sorts it by the number that is gotten from the dictionary
+                        ForEach(difficultiesDictionary.keys.sorted { lhs, rhs in
+                            difficultiesDictionary[lhs]! < difficultiesDictionary[rhs]!
+                        }, id: \.self) { difficulty in
+                            Button {
+                                difficultySelected = difficulty
+                            } label: {
+                                Text(difficulty)
+                            }
+                        }
+                    } label: {
+                        HStack{
+                            Text(difficultySelected)
+                                .foregroundStyle(.blue)
+                            
+                            Image(systemName: "arrowshape.down.fill")
+                                .resizable()
+                                .frame(maxWidth: 20, maxHeight: 20)
+                                .foregroundStyle(.blue)
+                        }
+                    }
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .frame(maxWidth: 190, maxHeight: 150)
+                                .foregroundStyle(.cyan)
+                            
+                            Text("Single player")
+                                .foregroundStyle(.black)
+                                .font(.title)
+                                .bold()
+                        }
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .frame(maxWidth: 190, maxHeight: 150)
+                                .foregroundStyle(.cyan)
+                            
+                            Text("Single player")
+                                .foregroundStyle(.black)
+                                .font(.title)
+                                .bold()
+                        }
+                    }
+
+                }
+                
+                Spacer()
             }
         }
     }
